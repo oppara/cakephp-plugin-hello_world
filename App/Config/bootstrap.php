23b8015f3f52141a74c7348ba1d9a732ca5a69ed
oppara <oppara@oppara.tv>
2016-08-21 11:18:41 +0900

    Bake HelloWorld plugin

--- App/Config/bootstrap.php
+++ App/Config/bootstrap.php
@@ -101,3 +101,5 @@ CakeLog::config('error', array(
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
+
+CakePlugin::load('HelloWorld', array('bootstrap' => false, 'routes' => false));
